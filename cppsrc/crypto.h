950af517ccfcbb06dd1c3d5fe87409a4aaebecfe
Daniel Shteinbok <dshteinbok@gmail.com>
2020-07-31 13:56:19 -0400

    first commit

---
+++ cppsrc/crypto.h
@@ -0,0 +1,2 @@
+#include <napi.h>
+
---
+++ index.js
@@ -0,0 +1,11 @@
+
+// establish SMTP connection
+
+// receive header
+function receiveHeader(addr, key, 
+
+// edit contact
+
+// retrieve file
+
+// yeet file up the chain to the MDA
